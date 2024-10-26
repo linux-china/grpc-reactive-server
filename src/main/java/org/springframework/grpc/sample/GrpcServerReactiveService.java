a00ab5a0ef3aee0e2cdbb52800b58899203b5e45
Libing Chen <libing.chen@gmail.com>
2024-10-26 11:08:29 +0800

    initial version

---
+++ src/main/java/org/springframework/grpc/sample/GrpcServerReactiveService.java
@@ -0,0 +1,48 @@
+package org.springframework.grpc.sample;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.grpc.sample.proto.HelloReply;
+import org.springframework.grpc.sample.proto.HelloRequest;
+import org.springframework.grpc.sample.proto.ReactorSimpleGrpc;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class GrpcServerReactiveService extends ReactorSimpleGrpc.SimpleImplBase {
+
+	private static Log log = LogFactory.getLog(GrpcServerReactiveService.class);
+
+	@Override
+	public Mono<HelloReply> sayHello(HelloRequest req) {
+		log.info("Hello " + req.getName());
+		HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+		return Mono.just(reply);
+	}
+
+	@Override
+	public Flux<HelloReply> streamHello(HelloRequest req) {
+		log.info("Hello " + req.getName());
+		return Flux.create(sink -> {
+			int count = 0;
+			while (count < 10) {
+				HelloReply reply = HelloReply.newBuilder()
+					.setMessage("Hello(" + count + ") ==> " + req.getName())
+					.build();
+				sink.next(reply);
+				count++;
+				try {
+					Thread.sleep(1000L);
+				}
+				catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
+					sink.error(e);
+					return;
+				}
+			}
+			sink.complete();
+		});
+	}
+
+}
